835767787653c9fceee87e9d2426a2a5f6651066
Alicia Amador <alicia.amador.cabezali@gmail.com>
2022-03-03 22:38:49 +0100

    Commit inicial

---
+++ criptos/models.py
@@ -0,0 +1,19 @@
+import requests
+from . import API_KEY, URL_TASA_ESPECIFICA
+
+class APIError(Exception):
+    pass
+
+class CriptoValorModel:
+    def __init__(self,origen: str, destino: str):
+        self.origen = origen
+        self.destino = destino
+
+        self.tasa = 0.0
+    
+    def obtenerTasa(self):
+        self.respuesta = requests.get(URL_TASA_ESPECIFICA.format(self.origen, self.destino, API_KEY))
+
+        if self.respuesta.status_code != 200:
+            raise APIError(self.respuesta.json()["error"])
+        self.tasa = round(respuesta.json()["rate"],2)
